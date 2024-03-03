c366ff6376c905b8e47eee467c5ff9a650d3eca4
Pasindu Jayalal <95409421+PasinduJayalal@users.noreply.github.com>
2024-03-03 17:47:03 +0000

    duplicate_images_to_csv_python_script

--- di.py
+++ di.py
@@ -1,51 +1,56 @@
 import os
 import hashlib
-import csv
+import pandas as pd
 from pathlib import Path
+# import tkinter as tk
+# from tkinter import filedialog
 
 
 def find_duplicates(start_path):
     file_list = os.walk(start_path)
-
     unique = dict()
     duplicateimages = dict()
 
     for root, folders, files in file_list:
         for file in files:
-            path = Path(os.path.join(root, file))
-            fileHash = hashlib.md5(open(path, 'rb').read()).hexdigest()
-
-            if fileHash not in unique:
-                unique[fileHash] = [(path, os.path.getctime(path))]
-            else:
-                original_path, original_time = unique[fileHash][0]
-                duplicate_time = os.path.getctime(path)
-
-                unique[fileHash].append((path, duplicate_time))
-
-                if duplicate_time < original_time:
-                    duplicateimages.setdefault(fileHash, []).append((path, original_path))
+            if file.endswith(".jpg" or file.endswith(".png")):
+                path = Path(os.path.join(root, file))
+                fileHash = hashlib.md5(open(path, 'rb').read()).hexdigest()
+                if fileHash not in unique:
+                    unique[fileHash] = [(path, os.path.getctime(path))]
                 else:
-                    duplicateimages.setdefault(fileHash, []).append((original_path, path))
+                    original_path, original_time = unique[fileHash][0]
+                    duplicate_time = os.path.getctime(path)
+                    unique[fileHash].append((path, duplicate_time))
+                    if duplicate_time < original_time:
+                        duplicateimages.setdefault(fileHash, []).append((path, original_path))
+                    else:
+                        duplicateimages.setdefault(fileHash, []).append((original_path, path))
 
     return duplicateimages
 
 
 def main():
+    # root = tk.Tk()
+    # root.withdraw()
+    # path = filedialog.askdirectory(title="Select Directory")
     path = os.getcwd()
-
     duplicate_images = find_duplicates(path)
+    duplicates_data = []
 
     if duplicate_images:
-        with open("DuplicateImages.csv", mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Original", "Duplicate"])
-            #print("Duplicate Images:")
-            for hash_value, paths_list in duplicate_images.items():
-                for original_path, duplicate_path in paths_list:
-                    #print(f"Original: {original_path}\nDuplicate: {duplicate_path}\n")
-                    #writer.writerow([f"{original_path}", f"{duplicate_path}"])
-                    writer.writerow([f"{original_path}".replace("/workspaces", ""), f"{duplicate_path}".replace("/workspaces", "")])
+        for hash_value, paths_list in duplicate_images.items():
+            for original_path, duplicate_path in paths_list:
+                duplicates_data.append({
+                    'Original': f"![](https://raw.githubusercontent.com/kent-map/main{original_path})".replace(
+                        "/workspaces", "").replace("/main/images/", "/images/main/"),
+                    'Duplicate': f"![](https://raw.githubusercontent.com/kent-map/main{duplicate_path})".replace(
+                        "/workspaces", "").replace("/main/images/", "/images/main/")
+                })
+
+        df = pd.DataFrame(duplicates_data)
+
+        df.to_csv("DuplicateImages.csv", index=False)
     else:
         print("No duplicate images found.")
 
